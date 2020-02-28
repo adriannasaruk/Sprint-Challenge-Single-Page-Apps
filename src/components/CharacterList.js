ec481dace81330b321de3de5537a646a5676c471
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-02-28 08:56:52 -1000

    MVP I guess

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,59 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState ("");
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+      axios
+        .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+        .then(response => {
+
+          const character = response.data.results.filter
+          (character => character.name.toLowerCase().includes(query.toLowerCase())
+          );
+          console.log (response)
+          setData(character);
+          console.log(character);
+        })
+        .catch(error => {
+          console.log("Sorry no data", error);
+        });
+    }, [query]);
+
+    const handleInputChange = event => {
+      setQuery(event.target.value);
+    }
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Characters:</h2>
+      <form>
+        <p>Search by Name:</p>
+        <input
+        type = "text"
+        onChange = {handleInputChange}
+        value = {query}
+        name = "name"
+        />
+      </form>
+      {data.map(item => {
+        return (
+          <div>
+            
+          <CharacterCard key= {item.name}
+          name = {item.name}
+          species = {item.species}
+          />
+          </div>
+        )
+      })}
     </section>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,8 +1,26 @@
 import React from "react";
+import CharacterList from "./CharacterList"
+import {Route} from "react-router-dom";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+const NewStyle = styled.nav`
+background: black;
+color: white;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
 
 export default function WelcomePage() {
   return (
-    <section className="welcome-page">
+<div>
+    <Link to = "/CharacterList">Characters
+        </Link>
+         
+    <NewStyle className="welcome-page">
+      
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
@@ -11,6 +29,9 @@ export default function WelcomePage() {
           alt="rick"
         />
       </header>
-    </section>
+      
+      
+    </NewStyle>
+    </div>
   );
 }
